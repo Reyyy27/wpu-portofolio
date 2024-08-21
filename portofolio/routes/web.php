5badea36df5afce4a16fd0e7223cc1b786c7723e
Reyyy27 <kakreihan27@gmail.com>
2024-08-21 10:10:39 +0800

    memperbaiki tampilan index dan about

--- portofolio/routes/web.php
+++ portofolio/routes/web.php
@@ -3,5 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.index'); });
+
+Route::get('/about', function () {
+    return view('pages.about'); });
+
+Route::get('/gallery', function () {
+    return view('pages.gallery'); });
+    
+Route::get('/services', function () {
+    return view('pages.services'); });
+
+Route::get('/contact', function () {
+    return view('pages.contact'); });
